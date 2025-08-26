7c2be403725a426f48afdfe951532c25c35cb1db
Abdulkerim Jemal <abduljemal43@gmail.com>
2025-08-26 23:23:55 +0300

    fix build

---
+++ src/features/birth-service/api/birthApi.ts
@@ -0,0 +1,34 @@
+import { birthListEndpoint } from "@/common/utils/constants/EndPoints";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const birthApi = createApi({
+    reducerPath: "birthApi",
+    baseQuery: fetchBaseQuery({
+        baseUrl: "http://localhost:3000",
+    }),
+    endpoints: (builder) => ({
+        getBirthsList: builder.query<any, { page?: number; perPage?: number }>({
+            query: ({ page = 1, perPage = 10 } = {}) => {
+                return `${birthListEndpoint}?page=${page}&perPage=${perPage}`;
+            },
+        }),
+        getBirthBySlug: builder.query({
+            query: ({ id }) => {
+                return `${birthListEndpoint}/${id}`;
+            },
+        }),
+        submitForm: builder.mutation<any, Record<string, any>>({
+            query: (formData) => ({
+                url: birthListEndpoint,
+                method: "POST",
+                body: formData,
+            }),
+        }),
+    }),
+});
+
+export const {
+    useSubmitFormMutation,
+    useGetBirthsListQuery,
+    useGetBirthBySlugQuery,
+} = birthApi;
