9ffd7dcc35358c939b0e7b1b79a4fe4e16d0a57f
AbeDevSaga <2000amandanm@gmail.com>
2025-09-16 10:20:22 +0300

    "complete"

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -4,7 +4,7 @@ import HomePage from "./components/HomePage";
 import ServicePage from "./components/ServicePage";
 import RequirementPage from "./components/RequirementPage";
 import FAQ from "./components/FAQ";
-import AnnouncementPage from "./components/AnnouncementPage";
+import AnnouncementPage from "./components/fakeAnnouncement";
 
 function page() {
   return (
